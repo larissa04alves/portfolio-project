b68c6fb4e6b37d17a43122cc709acaeaea8ee080
Larissa Alves <alves04.larissa@gmail.com>
2025-11-26 13:38:00 -0300

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;600&family=Space+Grotesk:wght@400;500;600;700&display=swap"
+		rel="stylesheet"
+	/>
+	<title>Larissa Alves | Fullstack Dev</title>
+</svelte:head>
+
+{@render children()}
