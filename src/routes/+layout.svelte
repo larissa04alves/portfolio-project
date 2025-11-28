5c0911d306372fbeab467b531c3b55c7305761da
Larissa Alves <alves04.larissa@gmail.com>
2025-11-28 10:33:52 -0300

    feat: atualizar título e descrição da página

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,13 +8,17 @@
 </script>
 
 <svelte:head>
+	<title>Larissa Alves | Dev Fullstack</title>
+	<meta
+		name="description"
+		content="Dev FullStack | Larissa  - Desenvolvedora Fullstack especializada em criar aplicações web modernas e responsivas. Confira meus projetos e entre em contato."
+	/>
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
 	<link
 		href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;600&family=Space+Grotesk:wght@400;500;600;700&display=swap"
 		rel="stylesheet"
 	/>
-	<title>Larissa Alves | Fullstack Dev</title>
 </svelte:head>
 
 <div class="h-full w-full">
